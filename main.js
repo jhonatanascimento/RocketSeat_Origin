f79c010c5b9dbc8b25afebb94b22849e3617ddce
Jhonata Nascimento <87243089+jhonatanascimento@users.noreply.github.com>
2021-07-24 22:00:10 -0300

    Menu Tornar menu invisivel após clicar.

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const nav = document.querySelector("#header nav");
 const toggle = document.querySelectorAll("nav .toggle");
 
+//Abrir ee fehca o menu
 for (const element of toggle) {
   element.addEventListener("click", function () {
     nav.classList.toggle("show");
+  });
+}
+
+// quando clicar em um item, fechar o menu ou esconder
+const links = document.querySelectorAll("nav ul li .title");
+for (const link of links) {
+  link.addEventListener("click", function () {
+    nav.classList.remove("show");
   });
 }
