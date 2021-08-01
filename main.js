88ce387b88239096f1cdbfcbb54e61128c17bf9a
Jhonata Nascimento <87243089+jhonatanascimento@users.noreply.github.com>
2021-07-31 23:06:06 -0300

    Finalizado

--- main.js
+++ main.js
@@ -20,20 +20,51 @@ for (const link of links) {
 const header = document.querySelector("#header");
 const navHeight = header.offsetHeight;
 
-window.addEventListener("scroll", function () {
+function changeHeaderWhenScroll() {
   if (this.scrollY >= navHeight) {
     header.classList.add("scroll");
   } else {
     header.classList.remove("scroll");
   }
-});
+}
 
 const backToTopButton = document.querySelector(".back-to-top");
 
-window.addEventListener("scroll", function () {
+function backToTop() {
   if (window.scrollY >= 560) {
     backToTopButton.classList.add("show");
   } else {
     backToTopButton.classList.remove("show");
   }
+}
+
+//menu ativo
+
+const sections = document.querySelectorAll("main section[id]");
+function activeMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    const checkpointStart = checkpoint >= sectionTop;
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector("nav ul li a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", function () {
+  backToTop();
+  changeHeaderWhenScroll();
+  activeMenuAtCurrentSection();
 });
